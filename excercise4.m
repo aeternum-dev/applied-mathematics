72093b152940eb6a9af4e08b813691e6870c0eb9
aeternum-dev <109350125+aeternum-dev@users.noreply.github.com>
2023-02-17 06:49:34 +0100

    Upload scripts from the fiirst week

---
+++ excercise4.m
@@ -0,0 +1,13 @@
+% first loop
+a=0;
+for i=1:5
+    a=a+0.2;
+end
+a==1
+
+% second loop
+a=1;
+for i=1:5
+    a=a-0.2;
+end
+a==0
---
+++ exercise5.m
@@ -0,0 +1,9 @@
+k = 0
+while true
+    if 1 + 2^(k-1) == 1
+        break
+    end
+    k = k-1;
+end
+k
+eps(1)
